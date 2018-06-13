462e36546a1290a5870e21d9da9b5b3747c86a7e
loveleen <loveleen.nancy@gmail.com>
2018-06-13 20:56:09 +0530

    Validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -11,6 +13,18 @@ export class HomeComponent {
   model = new Employee('', '', true, '', 'default');
   hasPrimaryLanguageError=false;
 
+  constructor(private formPoster: FormPoster) {
+
+  }
+
+  submitForm(form: NgForm) {
+    //validate form
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if(this.hasPrimaryLanguageError)
+      return;
+
+    this.formPoster.postEmployeeForm(this.model);
+  }
   validatePrimaryLanguage(value) {
     if(value === 'default') {
       this.hasPrimaryLanguageError = true;
